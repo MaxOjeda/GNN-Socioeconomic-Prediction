4997df22010423705da125aca85ec1f168a17bbc
Max Ojeda <max.ojeda.aguila@gmail.com>
2023-08-30 20:42:11 -0400

    graphsage attrs

---
+++ Models/GraphSageAttr.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import Linear
+from typing import Union, Tuple
+from torch_geometric.typing import OptPairTensor, Adj, OptTensor, Size
+from torch_geometric.nn.conv import MessagePassing
+
+class SAGEConvAttr(MessagePassing):
+    def __init__(self, in_channels: Union[int, Tuple[int, int]],
+                 out_channels: int, normalize: bool = False,
+                 root_weight: bool = True,
+                 bias: bool = True, **kwargs):  # yapf: disable
+        kwargs.setdefault('aggr', 'mean')
+        super(SAGEConvAttr, self).__init__(**kwargs)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.normalize = normalize
+        self.root_weight = root_weight
+
+        if isinstance(in_channels, int):
+            in_channels = (in_channels, in_channels)
+
+        self.lin_l = Linear(in_channels[0], out_channels, bias=bias)
+        if self.root_weight:
+            self.lin_r = Linear(in_channels[1], out_channels, bias=False)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.lin_l.reset_parameters()
+        if self.root_weight:
+            self.lin_r.reset_parameters()
+
+
+    def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
+                edge_attr: OptTensor = None, size: Size = None) -> Tensor:
+        """"""
+        if isinstance(x, Tensor):
+            x: OptPairTensor = (x, x)
+
+        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
+        out = self.lin_l(out)
+
+        x_r = x[1]
+        if self.root_weight and x_r is not None:
+            out += self.lin_r(x_r)
+
+        if self.normalize:
+            out = F.normalize(out, p=2., dim=-1)
+
+        return out
+    
+
+class GraphSAGEAttr(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout):
+        super(GraphSAGEAttr,self).__init__()
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConvAttr(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConvAttr(hidden_channels, hidden_channels))
+        self.convs.append(SAGEConvAttr(hidden_channels, out_channels))
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, adj_t, edge_attr):
+        #edge_attr = torch.mm(edge_attr, emb_ea)
+        for conv in self.convs[:-1]:
+            x = conv(x, adj_t, edge_attr)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, adj_t, edge_attr)
+        return x
--- Models/Transformer.py
+++ Models/Transformer.py
@@ -1,48 +1,19 @@
 import torch
 import torch.nn.functional as F
-from torch.nn import Linear
 from torch_geometric.nn import TransformerConv
 
-class TransformerClassification(torch.nn.Module):
-    def __init__(self, input_channels, hidden_channels, output_channels, edge_dim=2, heads=1, dropout=0.5, num_layers=1):
-        super(TransformerClassification, self).__init__()
+class Transformer(torch.nn.Module):
+    def __init__(self, input_channels, hidden_channels, output_channels, edge_dim=2, heads=1, dropout=0.5, num_layers=2):
+        super(Transformer, self).__init__()
         self.dropout = dropout
         self.heads = heads
         self.edge_dim = edge_dim
         
         self.conv1 = TransformerConv(input_channels, hidden_channels, heads=self.heads, dropout=self.dropout, edge_dim=self.edge_dim)
         self.convs = torch.nn.ModuleList()
-        for _ in range(num_layers - 1):
+        for _ in range(num_layers - 2):
             self.convs.append(TransformerConv(hidden_channels*self.heads, hidden_channels*self.heads))
         self.conv2 = TransformerConv(hidden_channels*self.heads, output_channels, concat=False, dropout=self.dropout)
-
-    def forward(self, x, edge_index, edge_attr):
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        if self.edge_dim != None:
-            x = self.conv1(x, edge_index, edge_attr)
-        else:
-            x = self.conv1(x, edge_index)
-        x = F.elu(x)
-        for conv in self.convs:
-            x = F.elu(conv(x, edge_index))
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.conv2(x, edge_index)
-        return x
-    
-
-class TransformerRegression(torch.nn.Module):
-    def __init__(self, input_channels, hidden_channels, output_channels, edge_dim=2, heads=1, dropout=0.5, num_layers=1):
-        super(TransformerRegression, self).__init__()
-        self.dropout = dropout
-        self.heads = heads
-        self.edge_dim = edge_dim
-        
-        self.conv1 = TransformerConv(input_channels, hidden_channels, heads=self.heads, dropout=self.dropout, edge_dim=self.edge_dim)
-        self.convs = torch.nn.ModuleList()
-        for _ in range(num_layers - 1):
-            self.convs.append(TransformerConv(hidden_channels*self.heads, hidden_channels*self.heads))
-        self.conv2 = TransformerConv(hidden_channels*self.heads, output_channels, concat=False, dropout=self.dropout)
-        #self.linear = Linear(hidden_channels, output_channels)
 
     def forward(self, x, edge_index, edge_attr):
         x = F.dropout(x, p=self.dropout, training=self.training)
